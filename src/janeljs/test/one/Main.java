a7748eae27d13624235d9c842164df414c2c9c57
janeljs <jisunlim818@gmail.com>
2020-12-12 20:44:39 +0900

    Add janeljs.test.one

---
+++ src/janeljs/test/one/Main.java
@@ -0,0 +1,18 @@
+package janeljs.test.one;
+
+import java.util.Scanner;
+
+public class Main {
+	private static final String PROMPT = "> ";
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print(PROMPT);
+
+		String word = sc.next();
+		int n = sc.nextInt();
+		String direction = sc.next().toLowerCase();
+
+		sc.close();
+	}
+}
---
+++ src/janeljs/test/one/PushWords.java
@@ -0,0 +1,5 @@
+package janeljs.test.one;
+
+public class PushWords {
+
+}
