ee688a1fd186fa2c5b4d4695a17076c3f24e4c6f
janeljs <jisunlim818@gmail.com>
2020-12-13 07:20:22 +0900

    Add a constructor for the class FlatCube

--- src/janeljs/test/two/FlatCube.java
+++ src/janeljs/test/two/FlatCube.java
@@ -1,5 +1,22 @@
 package janeljs.test.two;
 
-public class FlatCube {
+import java.util.ArrayDeque;
+import java.util.Deque;
 
+public class FlatCube {
+	Deque<String> cubeTop, cubeMiddle, cubeBottom;
+	
+	public FlatCube() {
+		cubeTop = new ArrayDeque<>();
+		cubeMiddle = new ArrayDeque<>();
+		cubeBottom = new ArrayDeque<>();
+		
+		String[][] cube = {{"R","R","W"},{"G","C","W"},{"G","B","B"}};
+		
+		for (int i = 0; i<3; i++) {
+			cubeTop.add(cube[0][i]);
+			cubeMiddle.add(cube[1][i]);
+			cubeBottom.add(cube[2][i]);
+		}
+	}
 }
