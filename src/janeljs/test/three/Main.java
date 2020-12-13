17cb0c4b91d05afa9b7db18dc54a4e048dc8872b
janeljs <jisunlim818@gmail.com>
2020-12-13 15:45:33 +0900

    Implement a function measuring program execution time

--- src/janeljs/test/three/Main.java
+++ src/janeljs/test/three/Main.java
@@ -3,11 +3,18 @@ package janeljs.test.three;
 
 
 public class Main {
-
+    static long startTime, endTime, totalTime;
 
     public static void main(String[] args) {
+        startTime = System.nanoTime();
         Prompt p = new Prompt();
         p.runPrompt();
+        endTime   = System.nanoTime();
+        totalTime = endTime - startTime;
+        double seconds = (double) totalTime / 1_000_000_000.0;
+        long t = Math.round(seconds);
+        
+        System.out.printf("경과시간: %02d:%02d\n", t / 60, t % 60);
         System.out.println("조작갯수: " + Cube.count);
         System.out.println("이용해주셔서 감사합니다. 뚜뚜뚜.");
     }
