c5fdb1c39116a5f17ae4e7f2aeb8461ec92da7a5
janeljs <jisunlim818@gmail.com>
2020-12-13 15:20:20 +0900

    Add getCommandKeyList method
    Add getCommandKeyList method which returns an ArrayList cmdQueue.
    Each Command(e.g. "F", "R") is saved as an element in cmdQueue.

--- src/janeljs/test/three/Prompt.java
+++ src/janeljs/test/three/Prompt.java
@@ -1,5 +1,44 @@
 package janeljs.test.three;
 
-public class Prompt {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Prompt {
+    
+    boolean isInteger(String cmd) {
+        try {
+
+            Integer.parseInt(cmd);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+    
+    ArrayList<String> getCommandKeyList(Scanner sc) {
+        String cmd = sc.nextLine();
+        ArrayList<String> cmdQueue = new ArrayList<>();
+        int countSingleQuote = 0;
+        for (int i = 0; i < cmd.length(); i++) {
+            String cmdKey = Character.toString(cmd.charAt(i)).toUpperCase();
+
+            if (cmdKey.equals("'")) {
+                countSingleQuote++;
+                cmdQueue.set(i - countSingleQuote, cmdQueue.get(i - countSingleQuote) + "'");
+                continue;
+            }
+            
+            int n = 0;
+            if (isInteger(cmdKey)) n = Integer.parseInt(cmdKey);
+
+            for (int j = 0; j < n - 1; j++) {
+                cmdQueue.add(cmdQueue.get(i - 1 - countSingleQuote));
+            }
+
+            if (!isInteger(cmdKey)) cmdQueue.add(cmdKey);
+        }
+        return cmdQueue;
+    }
+    
+    
 }
