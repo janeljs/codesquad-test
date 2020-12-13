ad6b4e8c32753bb6096168b99aefd9a28a399756
janeljs <jisunlim818@gmail.com>
2020-12-13 16:22:00 +0900

    Refactor checkAnswer method

--- src/janeljs/test/three/Cube.java
+++ src/janeljs/test/three/Cube.java
@@ -48,18 +48,27 @@ public class Cube {
     boolean checkAnswer() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (cubeLeft[i][j].equals("W")==false) return false;
-                if (cubeFront[i][j].equals("O")==false) return false;
-                if (cubeRight[i][j].equals("G")==false) return false;
-                if (cubeBack[i][j].equals("Y")==false) return false;
-                if (cubeUp[i][j].equals("B")==false) return false;
-                if (cubeDown[i][j].equals("R")==false) return false;
+                checkSide(i, j);
             }
         }
         return true;
     }
-    
- 
+
+    boolean checkSide(int i, int j) {
+        if (cubeLeft[i][j].equals("W") == false)
+            return false;
+        if (cubeFront[i][j].equals("O") == false)
+            return false;
+        if (cubeRight[i][j].equals("G") == false)
+            return false;
+        if (cubeBack[i][j].equals("Y") == false)
+            return false;
+        if (cubeUp[i][j].equals("B") == false)
+            return false;
+        if (cubeDown[i][j].equals("R") == false)
+            return false;
+        return true;
+    }
 
     void rotateCube(String cmd) {
         Map<String, Runnable> commands = new HashMap<>();
