299c96c978a2ed34f448e9c3d95692c5c90374a8
janeljs <jisunlim818@gmail.com>
2020-12-13 13:51:41 +0900

    Add rotateClockwise and rotateCounterclockwise method to rotate two dimensional array

--- src/janeljs/test/three/Cube.java
+++ src/janeljs/test/three/Cube.java
@@ -3,7 +3,8 @@ package janeljs.test.three;
 public class Cube {
 
     String[][] cubeLeft, cubeFront, cubeRight, cubeUp, cubeBack, cubeDown;
-
+    static int count = 0;
+    
     public Cube() {
         cubeLeft = new String[3][3];
         cubeFront = new String[3][3];
@@ -23,10 +24,32 @@ public class Cube {
                 cubeBack[i][j] = "Y";
                 cubeUp[i][j] = "B";
                 cubeDown[i][j] = "R";
-
             }
         }
-
     }
+    
+    String[][] rotateClockwise(String[][] cubeC) {
+        String[][] updatedCube = new String[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                updatedCube[j][2 - i] = cubeC[i][j];
+            }
+        }
+        return updatedCube;
+    }
+    
+    String[][] rotateCounterclockwise(String[][] cubeC) {
+        String[][] updatedCube = new String[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                updatedCube[2 - j][i] = cubeC[i][j];
+            }
+        }
+        return updatedCube;
+    }
+
+    
+   
+
 
 }
