2419ba1914470dcc9eda895dbe8cdff066a0de4d
janeljs <jisunlim818@gmail.com>
2020-12-13 09:36:03 +0900

    Simplify Main.java using class Prompt

--- src/janeljs/test/two/Main.java
+++ src/janeljs/test/two/Main.java
@@ -1,18 +1,9 @@
 package janeljs.test.two;
 
-import java.util.Scanner;
-
 public class Main {
-	private static final String PROMPT = "> ";
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print(PROMPT);
-
-		String word = sc.next();
-		int n = sc.nextInt();
-		String direction = sc.next().toLowerCase();
-
-		sc.close();
+		Prompt p = new Prompt();
+		p.runPrompt();
 	}
 }
