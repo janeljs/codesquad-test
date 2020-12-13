d09cb3f4ddf55640c620a8af5ceb2f238e1fe8a3
janeljs <jisunlim818@gmail.com>
2020-12-13 11:40:35 +0900

    Fix Indentation

--- src/janeljs/test/one/Main.java
+++ src/janeljs/test/one/Main.java
@@ -3,23 +3,25 @@ package janeljs.test.one;
 import java.util.Scanner;
 
 public class Main {
-	private static final String PROMPT = "> ";
+    private static final String PROMPT = "> ";
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print(PROMPT);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(PROMPT);
 
-		String word = sc.next();
-		int n = sc.nextInt();
-		String direction = sc.next().toLowerCase();
+        String word = sc.next();
+        int n = sc.nextInt();
+        String direction = sc.next().toLowerCase();
 
-		sc.close();
-		
-		PushWords pw = new PushWords(word);
+        sc.close();
 
-		if (pw.isLeft(n, direction)) pw.pushLeft(n);
-		else pw.pushRight(n);
-		
-		pw.printWord(pw.deque);
-	}
+        PushWords pw = new PushWords(word);
+
+        if (pw.isLeft(n, direction))
+            pw.pushLeft(n);
+        else
+            pw.pushRight(n);
+
+        pw.printWord(pw.deque);
+    }
 }
--- src/janeljs/test/one/PushWords.java
+++ src/janeljs/test/one/PushWords.java
@@ -4,39 +4,39 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class PushWords {
-	Deque<Character> deque;
+    Deque<Character> deque;
 
-	public PushWords(String word) {
-		deque = new ArrayDeque<>();
-		for (int i = 0; i < word.length(); i++) {
-			deque.addLast(word.charAt(i));
-		}
-	}
+    public PushWords(String word) {
+        deque = new ArrayDeque<>();
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i));
+        }
+    }
 
-	boolean isLeft(int n, String direction) {
-		if ((n > 0 && direction.equals("l")) || (n < 0 && direction.equals("r")))
-			return true;
-		return false;
-	}
+    boolean isLeft(int n, String direction) {
+        if ((n > 0 && direction.equals("l")) || (n < 0 && direction.equals("r")))
+            return true;
+        return false;
+    }
 
-	void pushLeft(int n) {
-		int numOfMovements = Math.abs(n);
-		for (int i = 0; i < numOfMovements; i++) {
-			deque.addLast(deque.removeFirst());
-		}
-	}
+    void pushLeft(int n) {
+        int numOfMovements = Math.abs(n);
+        for (int i = 0; i < numOfMovements; i++) {
+            deque.addLast(deque.removeFirst());
+        }
+    }
 
-	void pushRight(int n) {
-		int numOfMovements = Math.abs(n);
-		for (int i = 0; i < numOfMovements; i++) {
-			deque.addFirst(deque.removeLast());
-		}
-	}
-	
-	void printWord(Deque<Character> deque) {
-		for (char x: deque) {
-			System.out.print(x);
-		}
-	}
+    void pushRight(int n) {
+        int numOfMovements = Math.abs(n);
+        for (int i = 0; i < numOfMovements; i++) {
+            deque.addFirst(deque.removeLast());
+        }
+    }
+
+    void printWord(Deque<Character> deque) {
+        for (char x : deque) {
+            System.out.print(x);
+        }
+    }
 
 }
